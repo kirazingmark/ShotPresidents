0e192826a96f123b493dd0b000d23120df535dd2
Unknown <1005698@student.sae.edu.au>
2017-03-06 18:04:54 +1000

    Minor tweaks for WebGL, started working on midground

---
+++ Assets/Scripts/MidgroundScroll.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MidgroundScroll : MonoBehaviour {
+
+    public float speed;
+    Renderer myRenderer;
+
+    void Start () {
+        myRenderer = GetComponent<Renderer>();
+    }
+
+    void FixedUpdate () {
+        myRenderer.material.mainTextureOffset += Vector2.right * speed * Time.fixedDeltaTime;
+    }
+}
