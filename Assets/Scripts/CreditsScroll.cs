04f81221d4639c3934697d13155c82827f103c7a
Kira Zingmark <1004568@student.sae.edu.au>
2017-03-15 18:00:17 +0800

    Credits Screen Implemented.
    - Credits Crawl setup.
    - Restart Button implemented.
    - Soundcloud Link Button implemented.

---
+++ Assets/Scripts/CreditsScroll.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsScroll : MonoBehaviour {
+
+    // VARIABLES AND CONSTANTS.
+    public float speed;
+    public float duration;
+    public Vector3 direction;
+
+    float elapsedTime = 0.0f;
+
+    //// Use this for initialization
+    //void Start () {
+
+    //}
+
+    // Update is called once per frame
+    void Update () {
+        // transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        if (elapsedTime < duration)
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+        }
+        else if (elapsedTime > duration)
+        {
+            speed = 0.0f;
+        }
+    }
+}
--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour {
 
     public GameObject pauseButton, pausePanel;
+    public int scene = SceneManager.GetActiveScene().buildIndex;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         OnUnPause();
 	}
 
@@ -27,6 +29,18 @@ public class Pause : MonoBehaviour {
     {
         pausePanel.SetActive(false);
         pauseButton.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    public void OnClick()
+    {
+        Application.OpenURL("https://soundcloud.com/hesherhenderson/psa-siri-knowsv3-wip/s-DvQAN");
+    
+    }
+
+    public void OnRestartClick()
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
         Time.timeScale = 1;
     }
 }
