354c8e872aeeed5ff05a9527f4ae243637fe55c8
Kira Zingmark <1004568@student.sae.edu.au>
2017-03-15 15:48:17 +0800

    Pause Menu Implemented.
    - Pause/Resume Sprites created.
    - Pause Script created and implemented.
    - Pause Panel & Canvas elements implemented.

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public GameObject pauseButton, pausePanel;
+
+	// Use this for initialization
+	void Start () {
+        OnUnPause();
+	}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+
+    public void OnPause()
+    {
+        pausePanel.SetActive(true);
+        pauseButton.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    public void OnUnPause()
+    {
+        pausePanel.SetActive(false);
+        pauseButton.SetActive(true);
+        Time.timeScale = 1;
+    }
+}
