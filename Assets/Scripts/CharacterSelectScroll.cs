376902a6f2ed883c6ab2c87b25832a33602759a3
Kira Zingmark <kirazingmark@gmail.com>
2017-03-19 12:26:13 +1000

    Menu Scene fully implemented.
    - PlayerPrefs setup to record Character Index int variable.
    - Character Selection Buttons implemented.
    - Character Selection Avatar Prefabs implemented.
    - Character Selection script implemented.
    - Moving UI Elements implemented.
    - Blinking Select Player Sprite implemented.
    - Moving Background and Blimp Sprite elements from Main Scene implemented in Menu Scene.
    - 8 second loop from WIP Track set on loop as Background Music.

---
+++ Assets/Scripts/CharacterSelectScroll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectScroll : MonoBehaviour {
+
+    // VARIABLES AND CONSTANTS.
+    public float speed;
+    public float duration;
+    public Vector3 direction;
+
+    float elapsedTime = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (elapsedTime < duration)
+        {
+            // Debug.Log("Test");
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+        }
+        else if (elapsedTime > duration)
+        {
+            speed = 0.0f;
+        }
+
+    }
+}
