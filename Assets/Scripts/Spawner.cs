bbe9943f9fc89478c9fa7e5c352a4eeb3680bcbc
Kira Zingmark <kirazingmark@gmail.com>
2017-03-23 20:23:24 +1000

    Player Spawning Update.
    - Created new Spawner script to manage spawning the Player initially, as well as spawning replacement Players once they are destroyed.
    - Initial Spawn function implemented, which instantiates the Player Prefab when the Main scene loads.
    - Created new Player Prefab.
    - Removed Player GameObject from the Project Heirachy.
    - Created duplicate Prefabs for Hesher & Jiggy Wiggy Players.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public Transform[] spawnLocations;
+    public GameObject[] whatToSpawnPrefab;
+    public GameObject[] whatToSpawnClone;
+
+    void Start () {
+        SpawnPlayer();
+    }
+
+    void SpawnPlayer() {
+        whatToSpawnClone[0] = Instantiate(whatToSpawnPrefab[0], spawnLocations[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+    }
+}
