4c1b747e66a5afe664d98131d1b176fd30e56c33
Unknown <1005698@student.sae.edu.au>
2017-03-24 09:44:37 +1000

    AWOL and JIGGYMIGGY prefabs now run their custom animations
    JFK setpiece for the intro is in
    Blimp timing modified slightly

---
+++ Assets/Scripts/SetPieceScroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPieceScroll : MonoBehaviour {
+
+    public float speed;
+    Transform myTransform;
+    public float minX = -16f;
+
+    void Start () {
+        myTransform = transform;
+    }
+
+    void FixedUpdate () {
+        myTransform.localPosition += Vector3.left * speed * Time.fixedDeltaTime;
+        if (myTransform.position.x < minX) {
+            Destroy(gameObject);
+        }
+    }
+}
