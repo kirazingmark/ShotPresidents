2fd53ce590bda925f79bad2834c2d2905a5bc430
Kira Zingmark <kirazingmark@gmail.com>
2017-03-23 22:10:53 +1000

    Player Respawn Implemented.
    - Functionality of KillPlayer script merged in Spawner script.
    - KillPlayer script removed.
    - DeathZoneTrigger GameObject removed from Heirarchy, with all necessary GameComponents duplicated in the Spawner.
    - 2DCollider for Spawner GameObject resized to match the original DeathZoneTrigger it replaced.
    - Transform Locations for Start Spawnpoint & Respawn Spawnpoint adjusted in the scene.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -13,8 +13,28 @@ public class Spawner : MonoBehaviour {
         SpawnPlayer();
     }
 
-    void SpawnPlayer() {
+    public void SpawnPlayer() {
 
         whatToSpawnClone[0] = Instantiate(whatToSpawnPrefab[0], spawnLocations[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+    }
+
+    public void SpawnNewPlayer1()
+    {
+
+        whatToSpawnClone[1] = Instantiate(whatToSpawnPrefab[1], spawnLocations[1].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+
+            Debug.Log(collision.gameObject.tag);
+            Destroy(collision.gameObject);
+
+            // An IF Statement should go in here to determine which Player was just destroyed, and call the function that spawns a different one.
+            SpawnNewPlayer1();
+        }
     }
 }
