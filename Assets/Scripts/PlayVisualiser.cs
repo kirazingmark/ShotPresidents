4ff15c65b30aa43cc8530c0e936dd471750378f1
Kira Zingmark <kirazingmark@gmail.com>
2017-03-24 13:32:51 +1000

    Alternative Visualiser Implemented & Fully Functional.
    - Alternative Visualiser set up on Quad in Main scene.
    - Created new PlayVisualiser script to play VisualiserVideo as soon as the Main scene starts.
    - New VisualiserVideo Material created to illuminate the VisualiserVideo.
    - Changed the Z Depth of the Blimp (brought it forward slightly).
    - VisualiserVideo staged to Preloaded Assets for the Project.
    - Pausing Videos is still a problem, addressing at the moment, next commit should address this.

---
+++ Assets/Scripts/PlayVisualiser.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class PlayVisualiser : MonoBehaviour {
+
+    public WebGLMovieTexture visualiser;
+    public string videoPath;
+
+    public void Awake() {
+        visualiser = new WebGLMovieTexture(videoPath);
+        gameObject.GetComponent<MeshRenderer>().material.mainTexture = visualiser;
+    }
+
+    public void Start() {
+        //StartCoroutine(WaitToPlay());
+        visualiser.Play();
+    }
+
+    //IEnumerator WaitToPlay() {
+    //    yield return new WaitForSeconds(0);
+        
+    //}
+
+    public void Update() {
+        visualiser.Update();
+    }
+
+    public void OnPause() {
+        visualiser.Pause();
+    }
+
+    public void OnUnPause() {
+        visualiser.Play();
+    }
+}
