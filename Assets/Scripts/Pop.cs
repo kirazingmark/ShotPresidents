ec59b0e7cc3b3f80654b1d11109833cd66038b73
DESKTOP-A59IMTJ\Zac <1005698@student.sae.edu.au>
2017-03-27 19:35:34 +1000

    Added robot
    Blimp crash is also prettier
    Only thing to add is second video clip

---
+++ Assets/Scripts/Pop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pop : MonoBehaviour {
+
+    public float startTime;
+    public float duration;
+    public GameObject anim;
+
+	void Start () {
+        duration += startTime;
+	}
+	
+	void Update () {
+		if (Time.time > startTime) {
+            anim.SetActive(true);
+        }
+        if (Time.time > duration) {
+            Destroy(gameObject);
+        }
+	}
+}
